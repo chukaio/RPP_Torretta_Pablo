790fb740f1f99d12b18c242fc2ffd1fa4357277b
chukaio <36230532+chukaio@users.noreply.github.com>
2020-10-13 18:15:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ switch ($path) {
         if ($method == 'POST') {
             //
             echo "Probando Postman";
-            echo "Testeando3";
+            echo "Testeando55";
         }
         else if ($method == 'GET'){
             //
